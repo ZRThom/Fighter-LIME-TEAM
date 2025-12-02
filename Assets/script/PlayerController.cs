5e8c1d7a4d6af74780d3d6a305b191e05a096742
LucasVe95 <lucasveysset1@gmail.com>
2025-12-02 15:35:11 +0100

    Add of the jump Need to fix the number of the jumb available

--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -1,54 +1,93 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour 
+public class PlayerController2D : MonoBehaviour 
 {
-    // --- PARTIE STATS ---
-    [Header("Stats du Joueur")]
-    [SerializeField] private string playerName;
-    [SerializeField] private int health = 100;
-    [SerializeField] private int score = 0;
+    [Header("Mouvement")]
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float crouchSpeed = 4f;
+    [SerializeField] private float jumpForce = 12f;
     
-    [Header("Équipement")]
-    [SerializeField] private string weapon;
+    [Header("Contrôles")]
+    public KeyCode jumpKey = KeyCode.Space; 
 
-    // --- PARTIE MOUVEMENT ---
-    [Header("Paramètres Mouvement")]
-    [SerializeField] private float speed = 5.0f;
-    [SerializeField] private float jumpHeight = 2f;
-    [SerializeField] private float gravity = -9.81f;
+    [Header("Accroupissement")]
+    [SerializeField] private Vector2 standingSize = new Vector2(1f, 1f); 
+    [SerializeField] private Vector2 standingOffset = Vector2.zero;      
+    [SerializeField] private Vector2 crouchSize = new Vector2(1f, 0.5f);   
+    [SerializeField] private Vector2 crouchOffset = new Vector2(0f, -0.25f); 
 
-    // Variables techniques
-    private CharacterController controller;
-    private Vector3 velocity;
+    [Header("Détection du Sol")]
+    [SerializeField] private Transform groundCheck; 
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer; 
+
+    private Rigidbody2D rb;
+    private BoxCollider2D col;
+    private bool isGrounded;
+    private bool isCrouching;
+    
+    // NOUVEAU : On compte les sauts
+    private int jumpCount = 0; 
+    private int maxJumps = 1; // Limite stricte à 1
 
     void Start() 
     {
-        controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+        col.size = standingSize;
+        col.offset = standingOffset;
     }
 
     void Update() 
-    { 
-        // 1. Gestion du sol (reset gravité)
-        if (controller.isGrounded && velocity.y < 0) 
+    {
+        // 1. Vérifier le sol
+        bool wasGrounded = isGrounded;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // Si on touche le sol, on remet le compteur à 0
+        if (isGrounded && rb.linearVelocity.y <= 0.1f) // Petite sécurité sur la vitesse Y
         {
-            velocity.y = -2f; 
+            jumpCount = 0;
         }
 
-        // 2. Déplacement (Z/Q/S/D)
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        // 2. Mouvement & Accroupissement
+        float x = Input.GetAxisRaw("Horizontal");
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
-
-        // 3. Saut
-        if (Input.GetButtonDown("Jump") && controller.isGrounded) 
+        if (isGrounded && (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)))
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            isCrouching = true;
+            col.size = crouchSize;
+            col.offset = crouchOffset;
+        }
+        else
+        {
+            isCrouching = false;
+            col.size = standingSize;
+            col.offset = standingOffset;
         }
 
-        // 4. Gravité
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime); 
+        float currentSpeed = isCrouching ? crouchSpeed : speed;
+        rb.linearVelocity = new Vector2(x * currentSpeed, rb.linearVelocity.y);
+
+        // 3. Saut (LOGIQUE MODIFIÉE)
+        // On vérifie si on n'a pas dépassé le nombre max de sauts
+        if (Input.GetKeyDown(jumpKey) && jumpCount < maxJumps && !isCrouching) 
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            jumpCount++; // On ajoute 1 au compteur
+        }
+
+        // 4. Flip
+        if (x > 0) transform.localScale = new Vector3(1, 1, 1);
+        else if (x < 0) transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 }
