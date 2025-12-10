b569a3d0695b61c2060da3b7fa165e2d582995db
Johann Paimboeuf <johann.paimboeuf@gmail.com>
2025-12-10 13:21:02 +0100

    adding sounds

--- Assets/script/AudioManager.cs
+++ Assets/script/AudioManager.cs
@@ -2,6 +2,20 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+ public static AudioManager instance; 
+ public void Awake()
+ {
+   if (instance != null)
+   {
+      Destroy(gameObject);
+   }
+   else
+   {
+      instance = this;
+      DontDestroyOnLoad(gameObject);
+   }
+ }
+
 
  [Header("-- Audio Source --")]
  [SerializeField] AudioSource musicSource;
@@ -13,6 +27,18 @@ public class AudioManager : MonoBehaviour
  public AudioClip background;
  public AudioClip testSFX;
 
+
+ // Cette fonction permet aux autres scripts de demander de jouer un son
+    public void PlaySFX(AudioClip clip)
+    {
+        // On vérifie qu'on a bien assigné la source dans l'inspecteur pour éviter un crash
+        if (SFXSource != null && clip != null)
+        {
+            SFXSource.PlayOneShot(clip);
+        }
+    }
+
+    
  private void Start()
  {
     musicSource.clip = background;
