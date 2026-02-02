78747b4d6465b08b38cd19f7d51c2f4dcfd1d872
Iles-AA <aitabbailes@gmail.com>
2026-02-02 17:06:10 +0100

    Health bar animation & Round Manager added

--- Assets/script/HealthBarTest.cs
+++ Assets/script/HealthBarTest.cs
@@ -1,26 +1,65 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections; 
 
 public class HealthBarTest : MonoBehaviour
 {
-    public Scrollbar healthScrollbar; 
-    public Image fillImage;           
-    
+    [Header("UI References")]
+    public Scrollbar healthScrollbar;
+    public Image fillImage;
+
+    [Header("Color Settings")]
     public Color highColor = Color.green;
     public Color mediumColor = Color.yellow;
     public Color lowColor = Color.red;
 
+    [Header("Animation Settings")]
+    public float animationSpeed = 5f;
+    private Coroutine currentAnimation;
+
     void Start()
     {
-        if(fillImage != null) fillImage.color = highColor;
+        if (fillImage != null && healthScrollbar != null) 
+        {
+            UpdateColor(); 
+        }
     }
 
-    public void SetDamages(float value)
+    public void SetDamages(float damageAmount)
     {
         if (healthScrollbar == null || fillImage == null) return;
 
-        healthScrollbar.size -= value;
+        float targetValue = healthScrollbar.size - damageAmount;
+    
+        if (targetValue < 0) targetValue = 0;
 
+        if (currentAnimation != null) StopCoroutine(currentAnimation);
+
+        currentAnimation = StartCoroutine(AnimateHealthBar(targetValue));
+    }
+
+
+    IEnumerator AnimateHealthBar(float targetValue)
+    {
+
+        while (Mathf.Abs(healthScrollbar.size - targetValue) > 0.001f)
+        {
+
+            healthScrollbar.size = Mathf.Lerp(healthScrollbar.size, targetValue, Time.deltaTime * animationSpeed);
+            
+            UpdateColor();
+
+            yield return null;
+        }
+
+
+        healthScrollbar.size = targetValue;
+        UpdateColor();
+    }
+
+
+    void UpdateColor()
+    {
         if (healthScrollbar.size > 0.5f)
         {
             fillImage.color = highColor;
@@ -33,5 +72,12 @@ public class HealthBarTest : MonoBehaviour
         {
             fillImage.color = lowColor;
         }
+    }
+
+    public void ResetHealth()
+    {
+        healthScrollbar.size = 1f;
+        UpdateColor();
+        Debug.Log("Health reset!");
     }
 }
