e73a8af273bbb573a76a2bd6c085165c9d3bb869
Thom <thomasmh64@gmail.com>
2026-02-05 12:33:34 +0100

    fix select player (multiple changes)

--- Assets/script/Selection_de_perso/CharacterSelectUI.cs
+++ Assets/script/Selection_de_perso/CharacterSelectUI.cs
@@ -14,23 +14,19 @@ public class CharacterSelectUI : MonoBehaviour
         PointerEventData eventData = (PointerEventData)Data;
         if (eventData.button == PointerEventData.InputButton.Left)
         {
-            if (p1Selected != null)
-            {
-                p1Selected.selectionMarkP1.SetActive(false);
-            }
+            if (p1Selected != null) p1Selected.selectionMarkP1.SetActive(false);
             selectionMarkP1.SetActive(true);
             p1Selected = this;
+
             GameManager.Instance.firstSelectedPrefab = characterPrefab;
         }
         else if (eventData.button == PointerEventData.InputButton.Right)
         {
-            if (p2Selected != null)
-            {
-                p2Selected.selectionMarkP2.SetActive(false);
-            }
+            if (p2Selected != null) p2Selected.selectionMarkP2.SetActive(false);
             selectionMarkP2.SetActive(true);
             p2Selected = this;
-            GameManager.Instance.firstSelectedPrefab = characterPrefab;
+
+            GameManager.Instance.secondSelectedPrefab = characterPrefab;
         }
         Debug.Log($"Click {eventData.button} on {characterPrefab.name}");
     }
