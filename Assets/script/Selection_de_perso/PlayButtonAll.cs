193eb55bf00012075fae85b9bd0563f10cbcc1de
Thom <thomasmh64@gmail.com>
2026-02-05 16:12:51 +0100

    fix unity character

--- Assets/script/Selection_de_perso/PlayButtonAll.cs
+++ Assets/script/Selection_de_perso/PlayButtonAll.cs
@@ -4,13 +4,15 @@ using UnityEngine.SceneManagement;
 public class PlayButtonAll : MonoBehaviour
 {
     public string targetSceneBase;
+    public string loadingScene = "SceneLoading";
     public void Play()
     {
-    if (GameManager.Instance.player1Prefab == null || GameManager.Instance.player2Prefab == null)
+    if (GameManager.Instance.firstSelectedPrefab == null || GameManager.Instance.secondSelectedPrefab == null)
         {
             Debug.Log("Players not selected");
             return;
         }
-        SceneManager.LoadScene(targetSceneBase);
+        SceneCompactLoader.sceneToLoad = targetSceneBase;
+        SceneManager.LoadScene(loadingScene);
     }
 }
